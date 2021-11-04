74aa3502a99fe45b23765d5438999c3dc9255328
heesuju <hsjoo2442@gmail.com>
2021-11-04 18:52:47 +0900

    feat 다중 발사체 기능 구현

--- Character.py
+++ Character.py
@@ -19,8 +19,11 @@ class Character(Object):
         self.missile_size = missile_size
         self.missile_velocity = missile_velocity
         self.fire_interval = fire_interval
-        self.missile_sfx = missile_sfx
+        self.missile_sfx =  pygame.mixer.Sound(missile_sfx)
+        self.missile_sfx.set_volume(0.1)
+        
         self.unlocked = unlocked
+        self.fire_count = 3
 
     def update(self):
         self.missiles_to_be_del = []
@@ -52,12 +55,14 @@ class Character(Object):
             
     def shoot(self):
         self.last_fired = time.time()
-        missile = Missile(self.missile_img, self.missile_size, self.missile_velocity, self.fire_interval, self.missile_sfx)
-        missile.change_size(self.boundary[0]//30,self.boundary[1]//20)
-        missile.sfx.play()
-        missile.x = round(self.x + self.sx / 2 - missile.sx / 2) 
-        missile.y = self.y - missile.sy - 1
-        self.missiles_fired.append(missile)
+        self.missile_sfx.play()
+        for num in range(1, self.fire_count+1):
+            missile = Missile(self.missile_img, self.missile_size, self.missile_velocity, self.fire_interval)
+            missile.change_size(self.boundary[0]//30,self.boundary[1]//20)
+            div_factor = self.fire_count + 1
+            missile.x = round((self.x + (num * (self.sx / div_factor))) - missile.sx / 2) 
+            missile.y = self.y - missile.sy - 1
+            self.missiles_fired.append(missile)
 
     def get_missiles_fired(self):
         return self.missiles_fired
