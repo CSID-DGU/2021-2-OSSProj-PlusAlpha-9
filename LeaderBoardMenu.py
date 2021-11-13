7dc4e90b9a51ec508395d186b387f223f7f1291d
seyoung <0112leesy@naver.com>
2021-11-13 23:03:23 +0900

    Modify LeaderBoardMenu.py

--- LeaderBoardMenu.py
+++ LeaderBoardMenu.py
@@ -31,7 +31,7 @@ class LeaderBoardMenu:
         self.menu.clear()
         self.menu.add.label("   - RANKING -   ", selectable=False)
         self.menu.add.button('     current ranking     ', self.current_rank)
-        self.menu.add.button('     past ranking     ', self.current_rank)
+        self.menu.add.button('     past ranking     ', self.past_rank)
         self.menu.add.button('         back         ', self.to_menu)
         self.menu.mainloop(self.screen)
 
@@ -43,14 +43,14 @@ class LeaderBoardMenu:
         self.menu.add.button('         back         ', self.rank)
         self.menu.draw(self.screen)
 
-    # def past_rank(self):
-    #     self.menu.clear()
-    #     self.menu.add.label("   - Past Rank -   ", selectable=False)
-    #     self.menu.add.button('     easy mode     ', current_easy_rank)
-    #     self.menu.add.button('     hard mode     ', current_hard_rank)
-    #     self.menu.add.button('         back         ', show_rank)
+    def past_rank(self):
+        self.menu.clear()
+        self.menu.add.label("   - Past Rank -   ", selectable=False)
+        self.menu.add.button('     easy mode     ', self.show_past_easy_rank)
+        self.menu.add.button('     hard mode     ', self.show_past_hard_rank)
+        self.menu.add.button('         back         ', self.rank)
 
-# 1. current rank 페이지화
+# 1. current rank 페이지화 O
 # 2. current rank 검색 기능 추가
 # 3. past rank 100위까지 자르고 정렬
 # 4. 데이터베이스에 날짜 속성 추가 (서버 타임스탬프 ??)
@@ -70,52 +70,122 @@ class LeaderBoardMenu:
 
             if(mode == 'easy'):
                 global easy_data
-                easy_data = rank.load_data("easy")
-                self.get_rank_page('easy',self.tens)
+                easy_data = rank.load_data('current','easy')
+                self.get_current_easy_rank_page(self.tens)
 
             elif(mode == 'hard'):
-                self.menu.clear()
-                self.menu.add.label("--Current Hard Rank--",selectable=False,font_size=30)
-                self.menu.add.label("ID      Score",selectable=False, font_size=20)
-                hard_data = rank.load_data("hard")
-                for i in range(len(hard_data)):
-                        easy_name = str(hard_data[i]['ID'])
-                        easy_score = '{0:>05s}'.format(str(hard_data[i]['score']))
-                        r= "#{} : ".format(i+1) + easy_name + "    " + easy_score
-                        self.menu.add.label(r,selectable=False, font_size=15)
-                self.menu.add.button('back', self.current_rank)
-                self.menu.mainloop(self.screen)
+                global hard_data
+                hard_data = rank.load_data('current','hard')
+                self.get_current_hard_rank_page(self.tens)
 
-    def get_rank_page(self, mode, tens):
+    def get_current_easy_rank_page(self, tens):
         self.menu.clear()
-        self.menu.add.label("--Current "+ mode +" Rank--",selectable=False,font_size=30)
-        self.menu.add.label("ID      Score",selectable=False, font_size=20)
+        self.menu.add.label("--Current Easy Rank--",selectable=False,font_size=30)
+        id_score_bar = "{:<15s}{:<30s}{:<20s}".format('Rank', 'ID', 'Score')
+        self.menu.add.label(id_score_bar,selectable=False, font_size=20)
         for i in range(10):
             if(tens*10+i == len(easy_data)): break
             name = str(easy_data[tens*10+i]['ID'])
             score = '{0:>05s}'.format(str(easy_data[tens*10+i]['score']))
-            r= "#{} : ".format(tens*10+i+1) + name + "    " + score
+            r = "{:<15s}{:<30s}{:<20s}".format(str(tens*10+i+1), name, score)
+            # r= "#{} : ".format(tens*10+i+1) + name + "    " + score
             self.menu.add.label(r,selectable=False, font_size=20)
         prev_next_frame = self.menu.add.frame_h(250, 60)
         if(tens == 0):
             prev_next_frame.pack(self.menu.add.label('  '),align=pygame_menu.locals.ALIGN_CENTER)
             prev_next_frame.pack(self.menu.add._horizontal_margin(150),align=pygame_menu.locals.ALIGN_CENTER)
-            prev_next_frame.pack(self.menu.add.button('>', self.get_next_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.button('>', self.get_next_easy_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
         elif(tens == len(easy_data)//10):
-            prev_next_frame.pack(self.menu.add.button('<', self.get_prev_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.button('<', self.get_prev_easy_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
             prev_next_frame.pack(self.menu.add._horizontal_margin(150),align=pygame_menu.locals.ALIGN_CENTER)
             prev_next_frame.pack(self.menu.add.label('  '),align=pygame_menu.locals.ALIGN_CENTER)
         else:
-            prev_next_frame.pack(self.menu.add.button('<', self.get_prev_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.button('<', self.get_prev_easy_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
             prev_next_frame.pack(self.menu.add._horizontal_margin(150),align=pygame_menu.locals.ALIGN_CENTER)
-            prev_next_frame.pack(self.menu.add.button('>', self.get_next_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.button('>', self.get_next_easy_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
         self.menu.add.button('back', self.current_rank)
         self.menu.mainloop(self.screen)
 
-    def get_next_rank_page(self):
+    def get_next_easy_rank_page(self):
         self.tens += 1
-        self.get_rank_page('easy', self.tens)
+        self.get_current_easy_rank_page(self.tens)
 
-    def get_prev_rank_page(self):
+    def get_prev_easy_rank_page(self):
         self.tens -= 1
-        self.get_rank_page('easy', self.tens)
+        self.get_current_easy_rank_page(self.tens)
+
+    def get_current_hard_rank_page(self, tens):
+        self.menu.clear()
+        self.menu.add.label("--Current Hard Rank--",selectable=False,font_size=30)
+        id_score_bar = "{:<15s}{:<30s}{:<20s}".format('Rank', 'ID', 'Score')
+        self.menu.add.label(id_score_bar,selectable=False, font_size=20)
+        for i in range(10):
+            if(tens*10+i == len(hard_data)): break
+            name = str(hard_data[tens*10+i]['ID'])
+            score = '{0:>05s}'.format(str(hard_data[tens*10+i]['score']))
+            r = "{:<15s}{:<30s}{:<20s}".format(str(tens*10+i+1), name, score)
+            # r= "#{} : ".format(tens*10+i+1) + name + "    " + score
+            self.menu.add.label(r,selectable=False, font_size=20)
+        prev_next_frame = self.menu.add.frame_h(250, 60)
+        if(tens == 0):
+            prev_next_frame.pack(self.menu.add.label('  '),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add._horizontal_margin(150),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.button('>', self.get_next_hard_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+        elif(tens == len(easy_data)//10):
+            prev_next_frame.pack(self.menu.add.button('<', self.get_prev_hard_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add._horizontal_margin(150),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.label('  '),align=pygame_menu.locals.ALIGN_CENTER)
+        else:
+            prev_next_frame.pack(self.menu.add.button('<', self.get_prev_hard_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add._horizontal_margin(150),align=pygame_menu.locals.ALIGN_CENTER)
+            prev_next_frame.pack(self.menu.add.button('>', self.get_next_hard_rank_page),align=pygame_menu.locals.ALIGN_CENTER)
+        self.menu.add.button('back', self.current_rank)
+        self.menu.mainloop(self.screen)
+
+    def get_next_hard_rank_page(self):
+        self.tens += 1
+        self.get_current_hard_rank_page(self.tens)
+
+    def get_prev_hard_rank_page(self):
+        self.tens -= 1
+        self.get_current_hard_rank_page(self.tens)
+
+    def show_past_easy_rank(self):
+        self.get_past_rank('easy')
+
+    def show_past_hard_rank(self):
+        self.get_past_rank('hard')
+
+    def get_past_rank(self, mode):
+            rank = Rank()
+            self.menu.clear()
+            self.menu.add.image("./Image/Catus.png", angle=-10, scale=(0.15, 0.15))
+
+            if(mode == 'easy'):
+                past_easy_data = rank.load_data('past','easy')
+                self.menu.add.label("--Past Easy Rank--",selectable=False,font_size=30)
+                id_score_bar = "{:<15s}{:<30s}{:<20s}".format('Rank', 'ID', 'Score')
+                self.menu.add.label(id_score_bar,selectable=False, font_size=20)
+                for i in range(100):
+                    if (i == len(past_easy_data)): break
+                    name = str(past_easy_data[i]['ID'])
+                    score = '{0:>05s}'.format(str(past_easy_data[i]['score']))
+                    r = "{:<15s}{:<30s}{:<20s}".format(str(i+1), name, score)
+                    # r= "#{} : ".format(tens*10+i+1) + name + "    " + scor
+                    self.menu.add.label(r,selectable=False, font_size=20)
+
+            elif(mode == 'hard'):
+                past_hard_data = rank.load_data('past','hard')
+                self.menu.add.label("--Past Hard Rank--",selectable=False,font_size=30)
+                id_score_bar = "{:<15s}{:<30s}{:<20s}".format('Rank', 'ID', 'Score')
+                self.menu.add.label(id_score_bar,selectable=False, font_size=20)
+                for i in range(100):
+                    if (i == len(past_hard_data)): break
+                    name = str(past_hard_data[i]['ID'])
+                    score = '{0:>05s}'.format(str(past_hard_data[i]['score']))
+                    r = "{:<15s}{:<30s}{:<20s}".format(str(i+1), name, score)
+                    # r= "#{} : ".format(tens*10+i+1) + name + "    " + scor
+                    self.menu.add.label(r,selectable=False, font_size=20)
+
+            self.menu.add.button('back', self.past_rank)
+            self.menu.mainloop(self.screen)
--- Rank.py
+++ Rank.py
@@ -1,6 +1,7 @@
 import pygame
 import pygame_menu
 import pymysql
+from datetime import datetime
 
 class Rank():
     def __init__(self):
@@ -12,20 +13,117 @@ class Rank():
             charset = 'utf8'
         )
 
-    def load_data(self,mode):                                             #데이터 베이스에서 데이터 불러오기
-            curs = self.score_db.cursor(pymysql.cursors.DictCursor)
-            if mode == "easy":
-                sql = "select * from easy_score order by score desc"
-            elif mode == "hard":
-                sql = "select * from hard_score order by score desc"
-            curs.execute(sql)
-            data = curs.fetchall()
-            curs.close()
-            return data
+    def load_data(self, term, mode):                                           #데이터 베이스에서 데이터 불러오기
+        curs = self.score_db.cursor(pymysql.cursors.DictCursor)
+        if term == 'current':
+            if mode == 'easy':
+                sql = 'select * from current_easy_score order by score desc'
+            elif mode == 'hard':
+                sql = 'select * from current_hard_score order by score desc'
 
-    def add_data(self, ID, score):                                   #데이터 베이스에서 데이터 추가하기
+        if term == 'past':
+            if mode == 'easy':
+                sql = 'select * from past_easy_score order by score desc'
+            elif mode == 'hard':
+                sql = 'select * from past_hard_score order by score desc'
+
+        curs.execute(sql)
+        data = curs.fetchall()
+        curs.close()
+        return data
+
+    def load_current_latest_data(self, mode):
+        curs = self.score_db.cursor(pymysql.cursors.DictCursor)
+        if mode == 'easy':
+            sql = 'select * from current_easy_score order by date desc'
+        elif mode == 'hard':
+            sql = 'select * from current_hard_score order by date desc'
+        curs.execute(sql)
+        data = curs.fetchall()
+        curs.close()
+        latest_data_date = str(data[0]['date'])
+        return latest_data_date
+
+    def add_data(self, term, mode, ID, score):                                   #데이터 베이스에서 데이터 추가하기
         curs = self.score_db.cursor()
-        sql = "INSERT INTO test_score (ID, score) VALUES (%s, %s)"
-        curs.execute(sql, (ID, score))
+        now = datetime.now()
+        if term == 'current':
+            if mode == 'easy':
+                sql = 'INSERT INTO current_easy_score (ID, score, date) VALUES (%s, %s, %s)'
+
+            if mode == 'hard':
+                sql = 'INSERT INTO current_hard_score (ID, score, date) VALUES (%s, %s, %s)'
+
+        if term == 'past':
+            if mode == 'easy':
+                sql = 'INSERT INTO past_easy_score (ID, score, date) VALUES (%s, %s, %s)'
+
+            if mode == 'hard':
+                sql = 'INSERT INTO past_hard_score (ID, score, date) VALUES (%s, %s, %s)'
+        curs.execute(sql, (ID, score, now.strftime('%Y-%m-%d')))
         self.score_db.commit()
-        curs.close()  
+        curs.close()
+
+    def check_ID(self, mode, ID):       # ID 중복 체크 (나중에 NULL 체크도 필요)
+        curs = self.score_db.cursor()
+        if mode == 'easy':
+            sql = 'select * from current_easy_score where ID = binary(%s)'
+        if mode == 'hard':
+            sql = 'select * from current_hard_score where ID = binary(%s)'
+        curs.execute(sql, (ID))
+        data = curs.fetchall()
+        curs.close()
+        if(len(data)>0): return 0   # 중복임
+        else: return 1              # 중복아님
+
+    
+    def clear_data(self, term, mode):   
+        curs = self.score_db.cursor(pymysql.cursors.DictCursor)
+        if term == 'current':
+            if mode == 'easy':
+                sql = 'delete from current_easy_score'
+            elif mode == 'hard':
+                sql = 'delete from current_hard_score'
+
+        if term == 'past':
+            if mode == 'easy':
+                sql = 'delete from past_easy_score'
+            elif mode == 'hard':
+                sql = 'delete from past_hard_score'
+
+        curs.execute(sql)
+        self.score_db.commit()
+        curs.close()
+
+    def move_data(self, mode):
+        data = self.load_data('current', mode)
+        for i in range(len(data)):
+            name = str(data[i]['ID'])
+            score = str(data[i]['score'])
+            self.add_data('past', mode, name, score)
+
+    def update_data(self):      # 랭킹 등록 시 date의 월 값이 달라질 때 update_data 호출 하면 될 듯
+        self.clear_data('past', 'easy')
+        self.move_data('easy')
+        self.clear_data('past', 'hard')
+        self.move_data('hard')
+        self.clear_data('current', 'easy')
+        self.clear_data('current', 'hard')
+
+
+# 데이터 삽입 기능 테스트 완료
+# r = Rank()
+# r.add_data('current','easy','test1',300)
+
+# 데이터 삭제 기능 테스트 완료
+# r = Rank()
+# r.clear_data('current', 'easy')
+
+# ID 중복 체크 기능 테스트 완료
+# r = Rank()
+# print(r.check_ID('easy','User1'))
+
+# 데이터 업데이트 (랭킹 갱신) 테스트 완료
+# r = Rank()
+# r.update_data()
+
