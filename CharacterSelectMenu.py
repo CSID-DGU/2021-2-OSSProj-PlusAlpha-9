7a570b199e30b703a89d78ab3a6b69d2385a92d5
jibminJung <jibminjung@gmail.com>
2021-11-17 14:16:51 +0900

    fix 메뉴 크기 조절 후 메뉴 이동시 사이즈 변경 안되던 것 수정

--- CharacterSelectMenu.py
+++ CharacterSelectMenu.py
@@ -16,6 +16,7 @@ class CharacterSelectMenu:
         # 화면 받고 화면 크기 값 받기
         self.screen = screen
         self.size = screen.get_size()
+        self.mainloop = True
         
         menu_image = pygame_menu.baseimage.BaseImage(image_path='./Image/StartImage.png',drawing_mode=pygame_menu.baseimage.IMAGE_MODE_FILL)
         mytheme = pygame_menu.themes.THEME_ORANGE.copy()
@@ -31,7 +32,7 @@ class CharacterSelectMenu:
         self.character_data = CharacterDataManager.load()
 
     def to_menu(self):
-        self.menu.disable()
+        self.mainloop = False
 
     #메뉴 구성하고 보이기
     def show(self):  
@@ -67,3 +68,35 @@ class CharacterSelectMenu:
         else:
             print("character locked")
 
+
+    def resizable_mainloop(self):
+        while self.mainloop:
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    break
+                if event.type == pygame.VIDEORESIZE:
+                    # Update the surface (min size : 300,500)
+                    self.screen = pygame.display.set_mode((max(event.w,300), max(event.h,500)),
+                                                    pygame.RESIZABLE)
+                    
+            #이전의 창크기와 다르다면 창크기가 변한것으로 인식하고 on_resize 실행
+            if (self.size != self.screen.get_size()):
+                self.on_resize()
+
+            # Draw the menu
+            self.menu.update(events)
+            self.menu.draw(self.screen)
+
+            pygame.display.flip()
+
+    def on_resize(self):
+        """
+        Function checked if the window is resized.
+        """
+        window_size = self.screen.get_size()
+        new_w, new_h = 1 * window_size[0], 1 * window_size[1]
+        self.menu.resize(new_w, new_h)
+        self.size = window_size
+        print(f'New menu size: {self.menu.get_size()}')
