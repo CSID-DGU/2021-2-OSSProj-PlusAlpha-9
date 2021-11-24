4f9350bbc35e87f8ade29f840bc0647c971772b4
jibminJung <jibminjung@gmail.com>
2021-11-24 16:21:37 +0900

    몹이 좌우로 움직이면서 내려오도록 수정

--- Mob.py
+++ Mob.py
@@ -1,15 +1,26 @@
 import pygame
 from Object import Object 
+from pygame.math import Vector2
+import math
 
 class Mob(Object):
     def __init__(self, img_path, size, velocity, missile):
         super().__init__(img_path, size, velocity)
         self.missile = missile
         self.is_targeted = False
+        self.direction = Vector2(1,1)
+        self.rad = 1
 
     def move(self, boundary, game):
-        if (game.size[0] != self.boundary[0]) or (game.size[1] != self.boundary[1]):
+        if (game.size[0] != self.boundary[0]) or (game.size[1] != self.boundary[1]): #update when screen resized
             self.on_resize(game.size)
-        self.y += self.velocity
+
+        self.x += self.direction.y
+        self.y += self.direction.x
+        self.rad+=0.05
+        self.direction.from_polar((self.velocity*3,math.sin(self.rad)*70))
+
         if self.y >= boundary[1] - self.sy:
             game.mobList.remove(self)
+
+        
