a4f443a99a4c66336e73fb2aad9dd29fa30564df
jibminJung <jibminjung@gmail.com>
2021-12-04 22:45:22 +0900

    주석 추가

--- boss/Gun.py
+++ boss/Gun.py
@@ -1,6 +1,6 @@
 
 
-class Gun():
-        def __init__(self,x,y):
+class Gun(): #Gun class for boss gun
+        def __init__(self,x,y): #need to save location
             self.x = x
             self.y = y
